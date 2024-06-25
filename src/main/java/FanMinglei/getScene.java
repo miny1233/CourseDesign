23a72acf52add4ff192b8aa18d96933ec331aee6
Wiggins0022 <862018985@qq.com>
2024-06-25 14:58:04 +0800

    前端页面优化
    文法为空时消息提示，分析句子时句子为空消息提示，开始分析后，提交按钮隐藏

--- src/main/java/FanMinglei/getScene.java
+++ src/main/java/FanMinglei/getScene.java
@@ -36,21 +36,26 @@ public class getScene {
         Button submit = new Button("提交");
         submit.setStyle("-fx-font-size: 14px;"); // 设置按钮的样式
 
-        // 将控件添加到VBox
         vBox.getChildren().addAll(label1, input, submit);
 
         Scene scene = new Scene(vBox, 500, 240);
 
         submit.setOnAction(event -> {
             String text = input.getText();
-            typeConversion.ConverseGrammar(text);
-            System.out.println("输入的文本内容是：" + text);
+            if(!text.isEmpty()){
+                typeConversion.ConverseGrammar(text);
+                System.out.println("输入的文本内容是：" + text);
+                Stage secondStage = new Stage();
+                Scene secondScene = getSecondScene();
+                secondStage.setTitle("预测分析器");
+                secondStage.setScene(secondScene);
+                secondStage.show();
+            }else {
+                Alert alertMessage = new Alert(Alert.AlertType.INFORMATION);
+                alertMessage.setContentText("文法不能为空");
+                alertMessage.show();
+            }
 
-            Stage secondStage = new Stage();
-            Scene secondScene = getSecondScene();
-            secondStage.setTitle("预测分析器");
-            secondStage.setScene(secondScene);
-            secondStage.show();
         });
 
         return scene;
@@ -123,34 +128,17 @@ public class getScene {
         TableView<Terminators> tableView = new TableView<>();
         tableView.setPrefHeight(200); // 设置TableView的高度
         Map<String, Terminators> terminatorsMap = typeConversion.getTerminatorsMap();
+        typeConversion.removeEmptyTerminators();
         Set<String> keySet = terminatorsMap.keySet();
-        for(String str: terminatorsMap.keySet()){
+        /*for(String str: terminatorsMap.keySet()){
             System.out.println("str = " + str);
-        }
+        }*/
+        tableView.getColumns().add(new TableColumn<>(""));
         for(String str : keySet){
             TableColumn column = new TableColumn(terminatorsMap.get(str).getVal());
             column.setMinWidth(80);
             tableView.getColumns().add(column);
         }
-
-        // 创建表格列
-       /* TableColumn<NonTerminators, String> empty = new TableColumn<>(" ");
-        TableColumn<NonTerminators, String> first = new TableColumn<>("+");
-        TableColumn<NonTerminators, String> second = new TableColumn<>("*");
-        TableColumn<NonTerminators, String> third = new TableColumn<>("(");
-        TableColumn<NonTerminators, String> fourth = new TableColumn<>(")");
-        TableColumn<NonTerminators, String> fifth = new TableColumn<>("i");*/
-
-        // 设置列宽度
-       /* empty.setMinWidth(80);
-        first.setMinWidth(80);
-        second.setMinWidth(80);
-        third.setMinWidth(80);
-        fourth.setMinWidth(80);
-        fifth.setMinWidth(80);*/
-
-        // 将列添加到TableView
-        //tableView.getColumns().addAll(empty, first, second, third, fourth, fifth);
 
         // 将标题和TableView添加到VBox
         vBox.getChildren().addAll(titleLabel, tableView);
@@ -169,26 +157,18 @@ public class getScene {
         titleLabel.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;"); // 设置标题的样式
 
         TableView<NonTerminators> tableView = new TableView<>();
-        tableView.setPrefHeight(200); // 设置TableView的高度
+        tableView.setPrefHeight(200);
 
-        // 创建表格列
-        TableColumn<NonTerminators, String> empty = new TableColumn<>(" ");
-        TableColumn<NonTerminators, String> first = new TableColumn<>("+");
-        TableColumn<NonTerminators, String> second = new TableColumn<>("*");
-        TableColumn<NonTerminators, String> third = new TableColumn<>("(");
-        TableColumn<NonTerminators, String> fourth = new TableColumn<>(")");
-        TableColumn<NonTerminators, String> fifth = new TableColumn<>("i");
+        Map<String, Terminators> terminatorsMap = typeConversion.getTerminatorsMap();
+        typeConversion.removeEmptyTerminators();
+        Set<String> keySet = terminatorsMap.keySet();
+        tableView.getColumns().add(new TableColumn<>(""));
+        for(String str : keySet){
+            TableColumn column = new TableColumn(terminatorsMap.get(str).getVal());
+            column.setMinWidth(80);
+            tableView.getColumns().add(column);
+        }
 
-        // 设置列宽度
-        empty.setMinWidth(80);
-        first.setMinWidth(80);
-        second.setMinWidth(80);
-        third.setMinWidth(80);
-        fourth.setMinWidth(80);
-        fifth.setMinWidth(80);
-
-        // 将列添加到TableView
-        tableView.getColumns().addAll(empty, first, second, third, fourth, fifth);
 
         // 将标题和TableView添加到VBox
         vBox.getChildren().addAll(titleLabel, tableView);
@@ -207,7 +187,7 @@ public class getScene {
         titleLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;"); // 设置标签的样式
 
         TextField inputSentence = new TextField();
-        inputSentence.setPromptText("在此输入句子"); // 设置提示文本
+        inputSentence.setPromptText("在此输入句子");
 
         Button submitButton = new Button("提交");
         submitButton.setStyle("-fx-font-size: 14px;"); // 设置按钮的样式
@@ -215,20 +195,33 @@ public class getScene {
         Label outputLabel = new Label("预测分析过程：");
         outputLabel.setStyle("-fx-font-size: 14px; -fx-font-weight: bold;"); // 设置输出标签的样式
 
+        Button step = new Button("单步");
+        step.setStyle("-fx-font-size: 14px;");
+        step.setLayoutX(500);
+        step.setLayoutY(500);
+
         TextArea processOutput = new TextArea();
         processOutput.setPrefHeight(200); // 设置TextArea的高度
         processOutput.setWrapText(true); // 自动换行
         processOutput.setEditable(false); // 设置为只读
 
-        // 设置按钮的点击事件
+        //提交后的事情
         submitButton.setOnAction(event -> {
             String sentence = inputSentence.getText();
-            String analysisProcess = "i进栈，用产生式F->i归约"; // 调用方法执行预测分析过程
-            processOutput.setText(analysisProcess); // 显示预测分析过程
+            if(!sentence.isEmpty()){
+                //隐藏button
+                submitButton.setVisible(false);
+                String analysisProcess = "i进栈，用产生式F->i归约"; // 调用方法执行预测分析过程
+                processOutput.setText(analysisProcess); // 显示预测分析过程
+            }else {
+                Alert alertMessage = new Alert(Alert.AlertType.INFORMATION);
+                alertMessage.setContentText("句子不能为空");
+                alertMessage.show();
+            }
         });
 
         // 将控件添加到VBox
-        vBox.getChildren().addAll(titleLabel, inputSentence, submitButton, outputLabel, processOutput);
+        vBox.getChildren().addAll(titleLabel, inputSentence, submitButton, outputLabel,step, processOutput);
 
         Scene scene = new Scene(vBox, 500, 400); // 调整场景大小以适应内容
         return scene;
